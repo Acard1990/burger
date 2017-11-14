6570cc3e304515ae971fc6ee8de29ea8c3f518d4
Scott Cardinali <ScottCardinali@lawn-143-215-126-242.lawn.gatech.edu>
2017-11-14 18:07:26 -0500

    built struct

---
+++ dB/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, eaten) VALUES
+  ("Bacon loaded Black Bean", true),
+  ("Pulled Pork with Slaw Double Decker", false),
+  ("Mac N Cheese Burger", false),
+  ("Cheese Burger", false);
